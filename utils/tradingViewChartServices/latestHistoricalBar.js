c97edd65a6dfddd4ac9073dbda663dd2dfba4a69
CrimzonSteel <jastyneraphaelanunciacion@gmail.com>
2025-05-30 16:58:56 +0700

    remove logs for latest historical bars

--- utils/tradingViewChartServices/latestHistoricalBar.js
+++ utils/tradingViewChartServices/latestHistoricalBar.js
@@ -1,14 +1,11 @@
 let latestHistoricalBar = null;
 
 function setNewLatestHistoricalBar(bar) {
-    console.log('trying to set new bar', bar);
     const convertedTime = bar?.time;
     if (!latestHistoricalBar) {
-        console.log('no bar set yet');
         latestHistoricalBar = bar;
     } else {
         if (convertedTime > latestHistoricalBar?.time) {
-            console.log('new bar more latest than old bar');
             latestHistoricalBar = bar;
         }
     }
