30753a629ab992f64453a171b279c20436a42ec7
codebyMoh <codebymohit@gmail.com>
2025-06-26 16:16:20 +0530

    hardik code to fixed images in memescop

--- components/common/memsscope/MemescopeImages.jsx
+++ components/common/memsscope/MemescopeImages.jsx
@@ -1,33 +1,38 @@
 "use client";
-import Image from 'next/image';
-import React, { memo, useState } from 'react'
+import Image from "next/image";
+import React, { memo, useState } from "react";
 
-const MemescopeImages = memo(({ showCircle, index, address, symbol, base_url }) => {
-    const [isImage, setIsImage] = useState(false);
+const MemescopeImages = memo(({ showCircle, address, symbol, base_url }) => {
+  const [imageStates, setImageStates] = useState({});
 
-    return (
-        isImage ? <div className={`absolute inset-0 m-auto flex items-center justify-center w-[64px] h-[64px] bg-[#3b3b49] object-cover ${showCircle ? "rounded-full" : ""
-            }`}>
-            <span className="text-white uppercase text-center text-xl">
-                {symbol?.charAt(0) || "T"}
-            </span>
-        </div> :
-            <Image
-                key={index + 1}
-                src={`${base_url}/${address}.webp`}
-                className={`absolute inset-0 m-auto w-[64px] h-[64px] object-cover ${showCircle ? "rounded-full" : ""
-                    }`}
-                width={64}
-                height={64}
-                alt="Profile"
-                loading="lazy"
-                onError={(e) => {
-                    setIsImage(true);
-                }}
-            />
-    )
-})
+  const isImageLoaded = imageStates[address] !== false;
 
-MemescopeImages.displayName = 'MemescopeImages';
-export default MemescopeImages
+  return isImageLoaded ? (
+    <Image
+      src={`${base_url}/${address}.webp`}
+      className={`absolute inset-0 m-auto w-[64px] h-[64px] object-cover ${
+        showCircle ? "rounded-full" : ""
+      }`}
+      width={64}
+      height={64}
+      alt="Profile"
+      loading="lazy"
+      onError={(e) => {
+        setImageStates((prev) => ({ ...prev, [address]: false }));
+      }}
+    />
+  ) : (
+    <div
+      className={`absolute inset-0 m-auto flex items-center justify-center w-[64px] h-[64px] bg-[#3b3b49] object-cover ${
+        showCircle ? "rounded-full" : ""
+      }`}
+    >
+      <span className="text-white uppercase text-center text-xl">
+        {symbol?.charAt(0) || "T"}
+      </span>
+    </div>
+  );
+});
 
+MemescopeImages.displayName = "MemescopeImages";
+export default MemescopeImages;
