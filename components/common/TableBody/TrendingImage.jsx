02e171f054c8d44c3047f7436a3c6c985ecc5288
codebyMoh <codebymohit@gmail.com>
2025-07-03 12:55:11 +0530

    set image height

--- components/common/TableBody/TrendingImage.jsx
+++ components/common/TableBody/TrendingImage.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const TrendingImage = ({ name, address, url }) => {
   const [isImageError, setIsImageError] = useState(false);
   return isImageError ? (
-    <div className="w-12 md:w-10 h-12 md:h-10 xl:w-14 xl:h-14 rounded-sm flex items-center justify-center bg-[#3b3b49] border border-[#1F73FC]">
+    <div className="w-12 md:w-10 h-12 md:h-10 xl:w-12 xl:h-12 rounded-sm flex items-center justify-center bg-[#3b3b49] border border-[#1F73FC]">
       <span className="text-sm text-white uppercase text-center">
         {name?.toString()?.slice(0, 1) || "T"}
       </span>
