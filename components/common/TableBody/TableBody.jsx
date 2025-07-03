9e2ccfb0ecbfce1da61868c5fca25c088a193b7b
codebyMoh <codebymohit@gmail.com>
2025-07-03 16:09:36 +0530

    testting trending image

--- components/common/TableBody/TableBody.jsx
+++ components/common/TableBody/TableBody.jsx
@@ -112,6 +112,7 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                   <td className="whitespace-nowrap w-60 md:px-6 px-3 py-2 ">
                     <div className="flex items-center gap-3 !text-left">
                       <TrendingImage
+                        key={`${row?.address}-${ind}`}
                         name={row?.name}
                         address={row?.address}
                         url={BASE_URL}
@@ -182,20 +183,20 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
 
                                 {(row?.offchainData?.extensions?.twitter ||
                                   row?.offchainData?.twitter) && (
-                                    <Link
-                                      href={
-                                        row?.offchainData?.extensions?.twitter ||
-                                        row?.offchainData?.twitter
-                                      }
-                                      target="_blank"
-                                      onClick={(e) => e.stopPropagation()}
-                                    >
-                                      <FaXTwitter
-                                        size={16}
-                                        className="text-[#6E6E6E] hover:text-[#ffffff]"
-                                      />
-                                    </Link>
-                                  )}
+                                  <Link
+                                    href={
+                                      row?.offchainData?.extensions?.twitter ||
+                                      row?.offchainData?.twitter
+                                    }
+                                    target="_blank"
+                                    onClick={(e) => e.stopPropagation()}
+                                  >
+                                    <FaXTwitter
+                                      size={16}
+                                      className="text-[#6E6E6E] hover:text-[#ffffff]"
+                                    />
+                                  </Link>
+                                )}
 
                                 {row?.offchainData?.extensions?.website && (
                                   <Link
@@ -251,10 +252,11 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                         </p>
                       </div>
                       <p
-                        className={`text-[12px] font-medium ${row?.Percentage < 0
-                          ? "text-[#ED1B24]"
-                          : "text-[#21CB6B]"
-                          }`}
+                        className={`text-[12px] font-medium ${
+                          row?.Percentage < 0
+                            ? "text-[#ED1B24]"
+                            : "text-[#21CB6B]"
+                        }`}
                       >
                         {`${row?.Percentage > 0 ? "+" : ""}${Number(
                           row?.Percentage || 0
@@ -301,8 +303,9 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                   </td> */}
                   <td className="whitespace-nowrap w-60 py-3 flex justify-center z-0">
                     <div
-                      className={`flex  gap-2 ${!row.mint_authority ? "text-white" : "text-[#828282]"
-                        }`}
+                      className={`flex  gap-2 ${
+                        !row.mint_authority ? "text-white" : "text-[#828282]"
+                      }`}
                     >
                       <Tooltip
                         body={
@@ -333,10 +336,11 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                       <Tooltip body={"No one can freeze token transfers."}>
                         <div className="grid  text-start">
                           <div
-                            className={`flex flex-col text-start opacity-75 ${!row.freeze_authority
-                              ? "text-white"
-                              : "text-[#828282]"
-                              }`}
+                            className={`flex flex-col text-start opacity-75 ${
+                              !row.freeze_authority
+                                ? "text-white"
+                                : "text-[#828282]"
+                            }`}
                           >
                             {!row.freeze_authority ? (
                               <CiCircleCheck
@@ -364,8 +368,9 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                       >
                         <div className="grid  text-start">
                           <div
-                            className={`flex flex-col text-start opacity-75 ${true ? "text-white" : "text-[#828282]"
-                              }`}
+                            className={`flex flex-col text-start opacity-75 ${
+                              true ? "text-white" : "text-[#828282]"
+                            }`}
                           >
                             {true ? (
                               <CiCircleCheck
@@ -393,8 +398,9 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                       >
                         <div className="grid  text-start">
                           <div
-                            className={`flex flex-col text-start opacity-75 ${row?.top10Holder ? "text-white" : "text-[#828282]"
-                              }`}
+                            className={`flex flex-col text-start opacity-75 ${
+                              row?.top10Holder ? "text-white" : "text-[#828282]"
+                            }`}
                           >
                             {row?.top10Holder ? (
                               <CiCircleCheck
@@ -420,8 +426,9 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                   {/* Column 6: Quick Buy Button */}
                   <td className="whitespace-nowrap w-32 py-2 place-items-center">
                     <button
-                      className={`text-[#111111] font-bold rounded-[20px] py-2 ${quickBuy > 0 ? "px-2" : "px-3"
-                        } bg-[#1d73fc] hover:bg-[#438bff] transition-all duration-300 ease-in-out flex items-center justify-center`}
+                      className={`text-[#111111] font-bold rounded-[20px] py-2 ${
+                        quickBuy > 0 ? "px-2" : "px-3"
+                      } bg-[#1d73fc] hover:bg-[#438bff] transition-all duration-300 ease-in-out flex items-center justify-center`}
                       onClick={(e) =>
                         buySolanaTokensQuickBuyHandler(
                           solanaLivePrice,
@@ -448,10 +455,11 @@ const TableBody = ({ isLoading, data, img, isTimeCreated, BASE_URL }) => {
                       </span>
                       <span>
                         {quickBuy > 0
-                          ? `${quickBuy.length > 6
-                            ? `Buy ${quickBuy.slice(0, 7)}... SOL`
-                            : `Buy ${quickBuy} SOL`
-                          }`
+                          ? `${
+                              quickBuy.length > 6
+                                ? `Buy ${quickBuy.slice(0, 7)}... SOL`
+                                : `Buy ${quickBuy} SOL`
+                            }`
                           : `Buy`}
                       </span>
                     </button>
