0b6cb05c72a02a05de2a1f42dd56ef5a5c580bcb
codebyMoh <codebymohit@gmail.com>
2025-08-19 17:27:54 +0530

    minor changes in withrtaw and buy sell

--- components/perpetuals/layout/Chart.jsx
+++ components/perpetuals/layout/Chart.jsx
@@ -3,84 +3,84 @@ import React, { useEffect, useRef, memo } from "react";
 import { widget } from "@/public/charting_library";
 import hyperliquidDatafeed from "@/utils/tradingViewChartServices/hyperliquidDatafeed";
 function Chart({ selectedSymbol }) {
-    const chartContainerRef = useRef(null); 
-    const tvWidgetRef = useRef(null);
+  const chartContainerRef = useRef(null);
+  const tvWidgetRef = useRef(null);
 
-    useEffect(() => {
-        if (!chartContainerRef.current) return;
-        if (tvWidgetRef.current) {
-            tvWidgetRef.current.remove();
-            tvWidgetRef.current = null;
-        }
+  useEffect(() => {
+    if (!chartContainerRef.current) return;
+    if (tvWidgetRef.current) {
+      tvWidgetRef.current.remove();
+      tvWidgetRef.current = null;
+    }
 
-        const Datafeed = hyperliquidDatafeed(selectedSymbol)
-        const widgetOptions = {
-            symbol: selectedSymbol,
-            interval: "1",
-            container: chartContainerRef.current,
-            library_path: "/charting_library/",
-            datafeed: Datafeed,
-            locale: "en",
-            fullscreen: false,
-            autosize: true,
-            timezone: "Asia/Kolkata",
-            theme: "dark",
-            toolbar_bg: "#08080E", 
-            loading_screen: { backgroundColor: "#08080E", foregroundColor: "#ffffff" },  
+    const Datafeed = hyperliquidDatafeed(selectedSymbol);
+    const widgetOptions = {
+      symbol: selectedSymbol,
+      interval: "1",
+      container: chartContainerRef.current,
+      library_path: "/charting_library/",
+      datafeed: Datafeed,
+      locale: "en",
+      disabled_features: [
+        "header_saveload",
+        "use_localstorage_for_settings",
+        "time_scale_controls",
+        "popup_hints",
+      ],
+      enabled_features: [
+        "study_templates",
+        "seconds_resolution",
+        "show_marks_on_series",
+        "cropped_tick_marks",
+        "end_of_period_timescale_marks",
+        "two_character_bar_marks_labels",
+      ],
+      charts_storage_url: "https://saveload.tradingview.com",
+      charts_storage_api_version: "1.1",
+      client_id: "tradingview.com",
+      fullscreen: false,
+      autosize: true,
+      timezone: "Asia/Kolkata",
+      theme: "dark",
+      toolbar_bg: "#08080E",
+      overrides: {
+        "paneProperties.backgroundGradientStartColor": "#08080e",
+        "paneProperties.backgroundGradientEndColor": "#08080e",
+        "paneProperties.vertGridProperties.color": "#1f1f1f",
+        "paneProperties.horzGridProperties.color": "#1f1f1f",
+        "scalesProperties.textColor": "#FFFFFF",
+        "mainSeriesProperties.candleStyle.wickUpColor": "#0a9980",
+        "mainSeriesProperties.candleStyle.wickDownColor": "#f33547",
+        "mainSeriesProperties.candleStyle.upColor": "#0a9980",
+        "mainSeriesProperties.candleStyle.downColor": "#f33547",
+        "mainSeriesProperties.candleStyle.borderUpColor": "#0a9980",
+        "mainSeriesProperties.candleStyle.borderDownColor": "#f33547",
+      },
+    };
 
-            overrides: {
-                 "paneProperties.background": "#08080E",
-                "paneProperties.backgroundType": "solid",
-                "paneProperties.vertGridProperties.color": "#1f1f1f",
-                "paneProperties.horzGridProperties.color": "#1f1f1f",
+    const tvWidget = new widget(widgetOptions);
+    window.tvWidget = tvWidget;
+    tvWidgetRef.current = tvWidget;
 
-                // Price scale and time scale
-                "scalesProperties.textColor": "#CCCCCC",
-                "scalesProperties.lineColor": "#1f1f1f",
+    tvWidget.onChartReady(() => {
+      console.log("Chart is ready");
+    });
 
-                // Candles
-                "mainSeriesProperties.candleStyle.upColor": "#0a9980",
-                "mainSeriesProperties.candleStyle.downColor": "#f33547",
-                "mainSeriesProperties.candleStyle.borderUpColor": "#0a9980",
-                "mainSeriesProperties.candleStyle.borderDownColor": "#f33547",
-                "mainSeriesProperties.candleStyle.wickUpColor": "#0a9980",
-                "mainSeriesProperties.candleStyle.wickDownColor": "#f33547",
+    return () => {
+      if (tvWidgetRef.current) {
+        tvWidgetRef.current.remove();
+        tvWidgetRef.current = null;
+      }
+    };
+  }, [selectedSymbol]);
 
-                // Crosshair
-                "crossHairProperties.color": "#888",
-            },
+  return (
+    <div
+      ref={chartContainerRef}
+      className="bg-[#08080E]"
+      style={{ backgroundColor: "#08080E", height: "100%", width: "100%" }}
+    />
+  );
+}
 
-            studies_overrides: {
-                // Volume indicator color
-                "volume.volume.color.0": "#f33547",
-                "volume.volume.color.1": "#0a9980",
-                "volume.volume.transparency": 80,
-                "volume.ma.linewidth": 1,
-                "volume.ma.color": "#888888",
-            },
-        };
-
-
-        const tvWidget = new widget(widgetOptions);
-        window.tvWidget = tvWidget;
-        tvWidgetRef.current = tvWidget;
-
-        tvWidget.onChartReady(() => {
-            console.log("Chart is ready");
-        });
-
-        return () => {
-            if (tvWidgetRef.current) {
-                tvWidgetRef.current.remove();
-                tvWidgetRef.current = null;
-            }
-        };
-    }, [selectedSymbol]);
-
-
-    return <div ref={chartContainerRef} className="bg-[#08080E]" style={{ backgroundColor: "#08080E", height: "100%", width: "100%", }} />;
-};
-
-export default memo(Chart);;
-
-
+export default memo(Chart);
--- components/perpetuals/popup/SwapPopup.jsx
+++ components/perpetuals/popup/SwapPopup.jsx
@@ -1,174 +1,202 @@
-import { Solana, usdcPerps } from '@/app/Images'
-import { ArrowUpDown, X } from 'lucide-react'
-import Image from 'next/image'
-import React, { useState } from 'react'
+import { Solana, usdcPerps } from "@/app/Images";
+import { ArrowUpDown, X } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { useDispatch, useSelector } from 'react-redux';
-import { showToaster, showToasterSuccess } from '@/utils/toaster/toaster.style';
-import axiosInstanceAuth from '@/apiInstance/axiosInstanceAuth';
-import { orderPositions } from '@/app/redux/perpetauls/perpetual.slice';
-import { showToastLoader } from '@/components/common/toastLoader/ToastLoder';
+import { useDispatch, useSelector } from "react-redux";
+import { showToaster, showToasterSuccess } from "@/utils/toaster/toaster.style";
+import axiosInstanceAuth from "@/apiInstance/axiosInstanceAuth";
+import { orderPositions } from "@/app/redux/perpetauls/perpetual.slice";
+import { showToastLoader } from "@/components/common/toastLoader/ToastLoder";
+import toast from "react-hot-toast";
 
-const SwapPopup = ({ onClose, perpsBalance, SolBalance }) => {
+const SwapPopup = ({ onClose, perpsBalance }) => {
+  const baseUrl = process.env.NEXT_PUBLIC_MOONPRO_BASE_URL;
+  const SOL_PRICE_IN_USDC = 170;
+  const activeSolWalletAddress = useSelector(
+    (state) => state?.userData?.activeSolanaWallet
+  );
 
-    const baseUrl = process.env.NEXT_PUBLIC_MOONPRO_BASE_URL
-    const SOL_PRICE_IN_USDC = 170;
-    const activeSolWalletAddress = useSelector(
-        (state) => state?.userData?.activeSolanaWallet
-    );
+  const dispatch = useDispatch();
+  const [solInputAmount, setSolInputAmount] = useState(0);
+  const [usdcInputAmount, setUsdcInputAmount] = useState(0);
+  const [btnLoading, setBtnLoading] = useState(false);
+  const userDetails = useSelector((state) => state?.userData?.userDetails);
 
-
-    const dispatch = useDispatch();
-    const [solInputAmount, setSolInputAmount] = useState(0);
-    const [usdcInputAmount, setUsdcInputAmount] = useState(0);
-    const [btnLoading, setBtnLoading] = useState(false)
-    const userDetails = useSelector((state) => state?.userData?.userDetails);
-
-
-    function handleSolInputAmount(e) {
-        const maxAmount = activeSolWalletAddress?.balance
-        let val = e.target.value
-        if (!/^\d*\.?\d*$/.test(val)) return;
-        if (Number(val) > maxAmount) {
-            val = maxAmount.toString();
-        }
-        setSolInputAmount(val);
-        setUsdcInputAmount(val ? (Number(val) * SOL_PRICE_IN_USDC).toFixed(2) : 0);
+  function handleSolInputAmount(e) {
+    const maxAmount = activeSolWalletAddress?.balance;
+    let val = e.target.value;
+    if (!/^\d*\.?\d*$/.test(val)) return;
+    if (Number(val) > maxAmount) {
+      val = maxAmount.toString();
     }
+    setSolInputAmount(val);
+    setUsdcInputAmount(val ? (Number(val) * SOL_PRICE_IN_USDC).toFixed(2) : 0);
+  }
 
-    const handleConfirmConvert = async () => {
-        try {
-            setBtnLoading(true)
-            showToastLoader("Transaction proccessing", "transation-toast");
-            const response = await axiosInstanceAuth.post(`${baseUrl}hyper/deposit`, {
-                amount: Number(solInputAmount),
-            })
-            dispatch(orderPositions(userDetails?.perpsWallet))
-            showToasterSuccess(response?.data?.message || "Successfully transfer");
-            setBtnLoading(false)
-            onClose(false);
-        } catch (error) {
-            showToaster(error?.response?.data?.message || "Please try again!");
-            setBtnLoading(false)
-        } finally {
-            setBtnLoading(false)
-        }
-    };
+  const handleConfirmConvert = async () => {
+    try {
+      setBtnLoading(true);
+      showToastLoader("Transaction proccessing", "transation-toast");
+      const response = await axiosInstanceAuth.post(`${baseUrl}hyper/deposit`, {
+        amount: Number(solInputAmount),
+      });
+      dispatch(orderPositions(userDetails?.perpsWallet));
+      toast.success(response?.data?.message || "Deposite successfull.", {
+        id: "transation-toast",
+        duration: 2000,
+      });
+      setBtnLoading(false);
+      onClose(false);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Please try again later!", {
+        id: "transation-toast",
+        duration: 5000,
+      });
+      setBtnLoading(false);
+    } finally {
+      setBtnLoading(false);
+    }
+  };
 
-    return (
-        <>
-            <motion.div
-                key="backdrop"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.2 }}
-                onClick={() => onClose()}
-                className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center !z-50"
+  return (
+    <>
+      <motion.div
+        key="backdrop"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        onClick={() => onClose()}
+        className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center !z-50"
+      >
+        <motion.div
+          key="modal"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0.8, opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          className="LanguagePopup-lg max-w-sm sm:w-[90%] border border-gray-800 w-full bg-[#08080E] rounded-md !z-50"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {/* Header */}
+          <div className="flex items-center justify-between p-2 border-b border-gray-800">
+            <h2 className="text-lg font-medium text-white">Exchange</h2>
+            <button
+              onClick={() => onClose()}
+              className="text-gray-400 hover:text-white"
             >
-                <motion.div
-                    key="modal"
-                    initial={{ scale: 0.8, opacity: 0 }}
-                    animate={{ scale: 1, opacity: 1 }}
-                    exit={{ scale: 0.8, opacity: 0 }}
-                    transition={{ duration: 0.2 }}
-                    className="LanguagePopup-lg max-w-sm sm:w-[90%] border border-gray-800 w-full bg-[#08080E] rounded-md !z-50"
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    {/* Header */}
-                    <div className="flex items-center justify-between p-2 border-b border-gray-800">
-                        <h2 className="text-lg font-medium text-white">Exchange</h2>
-                        <button
-                            onClick={() => onClose()}
-                            className="text-gray-400 hover:text-white"
-                        >
-                            <X size={20} />
-                        </button>
+              <X size={20} />
+            </button>
+          </div>
+          <div className="p-4">
+            <div className="flex flex-col h-full">
+              <div className="mb-2">
+                <div className=" bg-[#1D1E26] rounded-lg px-4 py-3 text-white text-sm">
+                  <div className="flex justify-between items-center">
+                    <div className="text-gray-400 text-xs font-medium">
+                      Converting
                     </div>
-                    <div className="p-4">
-                        <div className="flex flex-col h-full">
-
-                            <div className="mb-2">
-                                <div className=" bg-[#1D1E26] rounded-lg px-4 py-3 text-white text-sm">
-                                    <div className='flex justify-between items-center'>
-                                        <div className="text-gray-400 text-xs font-medium">Converting</div>
-                                        <div className="text-gray-400 text-xs font-medium">Balance: {SolBalance}</div>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <input
-                                            type="number"
-                                            value={solInputAmount}
-                                            placeholder={`0.0`}
-                                            onChange={handleSolInputAmount}
-                                            className="bg-transparent text-base  font-medium focus:outline-none placeholder-gray-400"
-                                        />
-                                        <div className='flex items-center gap-2 mt-2'>
-                                            <Image src={Solana} alt="usdc" height={20} width={20} className="rounded-full" />
-                                            <div className="text-gray-400 text-xs font-medium">SOL</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex justify-center my-2">
-                                <button
-                                    className="bg-[#2A2A2A] hover:bg-[#3A3A3A] p-2 rounded-full transition-colors"
-                                >
-                                    <ArrowUpDown size={16} className="text-white" />
-                                </button>
-                            </div>
-
-                            <div className="mb-6">
-                                <div className=" bg-[#1D1E26] rounded-lg px-4 py-3 text-white text-sm">
-                                    <div className='flex justify-between items-center'>
-                                        <div className="text-gray-400 text-xs font-medium">Gaining</div>
-                                        <div className="text-gray-400 text-xs font-medium">Balance: {perpsBalance}</div>
-                                    </div>
-                                    <div className="flex items-center justify-between">
-                                        <input
-                                            type="number"
-                                            placeholder={`0.0`}
-                                            value={usdcInputAmount}
-                                            disabled
-                                            className="bg-transparent text-base  font-medium focus:outline-none placeholder-gray-400"
-                                        />
-                                        <div className='flex items-center gap-2  mt-2'>
-                                            <Image src={usdcPerps} alt="usdc" height={20} width={20} className="rounded-full" />
-                                            <div className="text-gray-400 text-xs font-medium"> USDC</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="text-right text-[#A8A8A8] text-xs mt-2">
-                                    {`1 SOL ≈ ${SOL_PRICE_IN_USDC} USDC`}
-                                </div>
-                            </div>
-
-                            <div className='text-[#A8A8A8] text-[10px] mb-4 mt-4 break-all'>NOTE: Exchange Native Solana for USDC on Hyperliquid. The minimum deposit is 7 USDC.</div>
-
-
-                            <div className="mt-auto">
-                                {btnLoading ?
-                                    <button className="w-full bg-[#1d73fc] opacity-50 cursor-not-allowed py-3 rounded-lg font-bold text-sm text-[#FFFFFF] transition-colors">
-                                        <div className="flex justify-center py-2 items-center gap-2">
-                                            <div className="loaderPopup"></div>
-                                        </div>
-                                    </button>
-                                    :
-                                    <button
-                                        onClick={handleConfirmConvert}
-                                        className="w-full bg-[#1d73fc] hover:bg-[#438bff] py-3 rounded-lg font-bold text-sm text-[#FFFFFF] transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-                                        disabled={usdcInputAmount < 7 || btnLoading}
-                                    >
-                                        Confirm
-                                    </button>
-                                }
-                            </div>
-                        </div>
+                    <div className="text-gray-400 text-xs font-medium">
+                      Balance: {activeSolWalletAddress?.balance}
                     </div>
-                </motion.div>
-            </motion.div >
-        </>
-    )
-}
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <input
+                      type="number"
+                      value={solInputAmount}
+                      placeholder={`0.0`}
+                      onChange={handleSolInputAmount}
+                      className="bg-transparent text-base  font-medium focus:outline-none placeholder-gray-400"
+                    />
+                    <div className="flex items-center gap-2 mt-2">
+                      <Image
+                        src={Solana}
+                        alt="usdc"
+                        height={20}
+                        width={20}
+                        className="rounded-full"
+                      />
+                      <div className="text-gray-400 text-xs font-medium">
+                        SOL
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-export default SwapPopup
+              <div className="flex justify-center my-2">
+                <button className="bg-[#2A2A2A] hover:bg-[#3A3A3A] p-2 rounded-full transition-colors">
+                  <ArrowUpDown size={16} className="text-white" />
+                </button>
+              </div>
+
+              <div className="mb-6">
+                <div className=" bg-[#1D1E26] rounded-lg px-4 py-3 text-white text-sm">
+                  <div className="flex justify-between items-center">
+                    <div className="text-gray-400 text-xs font-medium">
+                      Gaining
+                    </div>
+                    <div className="text-gray-400 text-xs font-medium">
+                      Balance: {perpsBalance}
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <input
+                      type="number"
+                      placeholder={`0.0`}
+                      value={usdcInputAmount}
+                      disabled
+                      className="bg-transparent text-base  font-medium focus:outline-none placeholder-gray-400"
+                    />
+                    <div className="flex items-center gap-2  mt-2">
+                      <Image
+                        src={usdcPerps}
+                        alt="usdc"
+                        height={20}
+                        width={20}
+                        className="rounded-full"
+                      />
+                      <div className="text-gray-400 text-xs font-medium">
+                        {" "}
+                        USDC
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-right text-[#A8A8A8] text-xs mt-2">
+                  {`1 SOL ≈ ${SOL_PRICE_IN_USDC} USDC`}
+                </div>
+              </div>
+
+              <div className="text-[#A8A8A8] text-[10px] mb-4 mt-4 break-all">
+                NOTE: Exchange Native Solana for USDC on Hyperliquid. The
+                minimum deposit is 7 USDC.
+              </div>
+
+              <div className="mt-auto">
+                {btnLoading ? (
+                  <button className="w-full bg-[#1d73fc] opacity-50 cursor-not-allowed py-3 rounded-lg font-bold text-sm text-[#FFFFFF] transition-colors">
+                    <div className="flex justify-center py-2 items-center gap-2">
+                      <div className="loaderPopup"></div>
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    onClick={handleConfirmConvert}
+                    className="w-full bg-[#1d73fc] hover:bg-[#438bff] py-3 rounded-lg font-bold text-sm text-[#FFFFFF] transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={usdcInputAmount < 6 || btnLoading}
+                  >
+                    Confirm
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+};
+
+export default SwapPopup;
